ecdfc714ad7bc6bf003a811007856ccd50b2f73f
jag6 <jag@sulopa.com>
2020-08-26 12:08:13 +0530

    Initial Commit

---
+++ Test Suites/google_test_suite.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>google_test_suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>6b540fbf-b9f3-4678-b328-be1a2228560b</testSuiteGuid>
+   <testCaseLink>
+      <guid>811cfd57-e391-4373-863e-36b3ca6132a9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Google_test</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
